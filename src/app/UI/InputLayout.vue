daff1170fcf25c33cd6f50b1c6e021090ba2ec7a
Kirill Chetvertkov <k.chetvertkov@hybrid.ai>
2021-04-26 17:56:02 +0300

    base layout

---
+++ src/app/UI/InputLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card outlined>
+    <div>
+      цель кредита
+      <slot></slot>
+    </div>
+  </v-card>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    name: 'InputLayout'
+  })
+</script>
---
+++ src/app/components/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="lighten-3" >
+    <v-row no-gutters>
+      <v-col :align-self="'start'">
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          <v-row no-gutters>
+            <slot></slot>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    name: 'Layout'
+  })
+</script>
