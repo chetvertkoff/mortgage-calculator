53cd3dffe7901a09f4a6d97f71d9ed97a36d985d
Kirill Chetvertkov <k.chetvertkov@hybrid.ai>
2021-04-26 17:53:53 +0300

    base layout

---
+++ src/app/components/InputGroup.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-col class="col-6" :align-self="'start'">
+    <InputLayout name="input-component">
+      <SelectListUI />
+    </InputLayout>
+  </v-col>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import Component from "vue-class-component";
+  import InputLayout from "@/App/UI/InputLayout.vue";
+  import SelectListUI from "@/App/UI/SelectListUI.vue";
+
+  @Component({components: {InputLayout, SelectListUI}})
+  export default class InputGroup extends Vue {
+
+  }
+</script>
---
+++ src/app/components/Total.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-col class="col-6" :align-self="'start'">
+    <v-card outlined>1</v-card>
+  </v-col>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import Component from "vue-class-component";
+
+  @Component()
+  export default class Total extends Vue {}
+</script>
