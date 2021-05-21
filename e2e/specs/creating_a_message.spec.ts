853c13ebdc1995b0ef4aca45bd20efccaa92272a
Kirill Chetvertkoff <chetvertkoff.kirill@gmail.com>
2021-05-21 16:56:51 +0300

    test added

--- e2e/specs/creating_a_message.spec.ts
+++ e2e/specs/creating_a_message.spec.ts
@@ -5,32 +5,22 @@ describe("Calculator using", () => {
 
   it("change all inputs", () => {
     // list input 'Цель кредита'
-    // cy.get('.selectInput').click()
-    // cy.get('.listItem').eq(1).click()
+    cy.get('.selectInput').click()
+    cy.get('.listItem').eq(1).click()
     //checkbox input 'есть зп карта'
-    // cy.get('.switchInput').click()
-    // slider inputs
-      cy.get("[data-test='sliderTextField']").each(($el, i)=>{
-        cy.get("[data-test='sliderTextField']").eq(i).type("New message")
-          .clear()
-          .type('11')
-
-
-      })
-
-
+    cy.get('.switchInput').click()
   });
 
-  // it("should has error", () => {
-  //   cy.get("[data-test='sliderTextField']").each(($el, i)=>{
-  //     cy.get("[data-test='sliderTextField']").eq(i).type("New message")
-  //       .clear()
-  //       .type('11')
-  //   })
-  //
-  //   cy.get(".slider-input").eq(1)
-  //     .should('have.class', 'validate-error')
-  // });
+  it("should has error", () => {
+    cy.get("[data-test='sliderTextField']").each(($el, i)=>{
+      cy.get("[data-test='sliderTextField']").eq(i).type("New message")
+        .clear()
+        .type('11')
+    })
+
+    cy.get(".slider-input").eq(1)
+      .should('have.class', 'validate-error')
+  });
 
 
 });
