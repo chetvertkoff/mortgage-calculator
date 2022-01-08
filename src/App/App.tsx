fa11de480961caeecdd44ebfcd10c33e79fa197f
Kirill Chetvertkoff <chetvertkoff.kirill@gmail.com>
2022-01-08 19:51:33 +0300

    fixes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,8 +5,7 @@ import { CalculatorForm } from "@/App/components/forms/CalculatorForm";
 
 console.log('Hello world');
 
-const App:                                                                                                        React.FC = () => {
-
+const App: React.FC = () => {
 	return (
 		<CalculatorLayout
 			form={ <CalculatorForm /> }
